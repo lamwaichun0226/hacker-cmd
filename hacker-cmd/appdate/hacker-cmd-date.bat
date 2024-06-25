e88187196e013a4113cfc8f2511e3cb2a198541b
lamwaichun <lamwaichun2024@gmail.com>
2024-06-25 21:13:07 +0800

    Add files via upload

---
+++ hacker-cmd/appdate/hacker-cmd-date.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /...
+color a
+echo                                     -waring-
+echo -this is not a joke,is a date,stop pause ctrl+c ang tap y
+pause
+dir /s
---
+++ hacker-cmd/appdate/hacker-cmd.bat
@@ -0,0 +1,157 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdCyDJGyX8VAjFBdVQRODNWiyE7sgzOfs4eaIo0kOaOsyeYjeyfTGcqld40brFQ==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZkoaHUrTXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlXMbSXrZg==
+::ZQ05rAF9IAHYFVzEqQI4KRhfSRbCP2a+Zg==
+::eg0/rx1wNQPfEVWB+kM9LVsJDAyDP2C/FPgf5eyb
+::fBEirQZwNQPfEVWB+kM9LVsJDAyDP2Da
+::cRolqwZ3JBvQF1fEqQIZO1taQxDCPSuoA7QQqOD67OGCsA0cRPJzaIPe26eLYOQcqkvrYdY4xX8ajMMFH1tMcVKmbwQx6W8CvGuOMqc=
+::dhA7uBVwLU+EWHGL+kVwAxRaS2Q=
+::YQ03rBFzNR3SWATE3E0+L1t/QwqFXA==
+::dhAmsQZ3MwfNWATE+EM9aAxVRUSBNH60Zg==
+::ZQ0/vhVqMQ3MEVWAtB9wBjQ0
+::Zg8zqx1/OA3MEVWAtB9wBjQ0
+::dhA7pRFwIByZRRmt5wI+Jw8UTUSQOWq2Rr0d6+P+/aqGsl1RRO46eZzemrCBYO4c/gXxZpNtwnJTi4sYA1tVfxmuLg50o2FJswQ=
+::Zh4grVQjdCyDJGyX8VAjFBdVQRODNWiyE7sgzO3o5P6IsnEVVeE0fZ2W2bmKHOgS6U7hZ9su234UndMJbA==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+color a
+title hacker cmd
+echo answer your username
+set /p user=
+:1
+set /p input=C:\Users\lamwaichun\:
+if /i %input%== cls goto cls
+if /i %input%== hack goto hack
+if /i %input%== dir goto dir
+if /i %input%== help goto help
+if /i not %input%== echo %input% is a unknow command,tap help to know the right command & goto 1
+
+:dir 
+echo dir
+start C:\Users\%user%\desktop\hacker-cmd\appdate\hacker-cmd-date.bat
+goto 1
+
+:help
+echo hack          -hack ip
+echo help          -help
+echo dir           -see cmd dir
+goto 1
+
+:hack
+echo tcp sharing virus
+echo hack 
+echo                                                       -ip list-
+echo tomato system creat
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo                                                          -sever ip-
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo                                                      -poe swith or ap ip-
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo                                                           -rounter ip-         
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+echo %random%.%random%.%random%.%random%
+
+echo thanks microsoft give batfile to write program
+goto 1
+
+:cls
+cls
+goto 1
+
+
+
+
+
+
